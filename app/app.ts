1d1b9edb730270be62d7777df8e1d954bcf1fabd
Nicolas <contact@nicolas-perraut.Fr>
2016-08-26 12:50:41 +0200

    Enable angular prodMode on device
    Fix #6

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {Component, enableProdMode} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 
+let prodMode: boolean = !!window.hasOwnProperty('cordava');
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -14,6 +15,12 @@ export class MyApp {
   constructor(private platform: Platform) {
     this.rootPage = HomePage;
 
+    if (platform.is('ios')
+      || platform.is('android')
+      || platform.is('windows')) {
+      enableProdMode();
+    }
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -22,4 +29,4 @@ export class MyApp {
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp, [], {prodMode: prodMode});

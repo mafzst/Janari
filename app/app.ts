b92181457ab765e4b109a315422b1211c7b6cf23
Nicolas <n.perraut@gmail.com>
2016-08-31 23:20:57 +0200

    First trying, not working :(

--- app/app.ts
+++ app/app.ts
@@ -1,13 +1,29 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {StorageService} from './services/storage';
+import {
+  FIREBASE_PROVIDERS,
+  defaultFirebase
+} from 'angularfire2';
 import {HomePage} from './pages/home/home';
 
 let prodMode: boolean = !!window.hasOwnProperty('cordava');
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  providers: [
+    FIREBASE_PROVIDERS,
+    defaultFirebase({
+      apiKey: "AIzaSyBGv-sxpWTwtfs3pO8DpS0lOnMhDvctoYY",
+      authDomain: "janari-4f29d.firebaseapp.com",
+      databaseURL: "https://janari-4f29d.firebaseio.com",
+      storageBucket: "janari-4f29d.appspot.com",
+    })
+    // firebaseAuthConfig({
+    //   provider: AuthProviders.Password,
+    //   method: AuthMethods.Password
+    // })
+  ]
 })
 export class MyApp {
 

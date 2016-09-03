50a0b1c8e32d438528035e77100e6688a012533b
Nicolas <n.perraut@gmail.com>
2016-09-03 22:03:20 +0200

    Implemet nutritional values

--- app/pages/details/details.ts
+++ app/pages/details/details.ts
@@ -16,8 +16,16 @@ export class DetailsPage {
     'salt': "Sel",
     'fat': "Matières grasses / Lipides",
     'saturated-fat': "Acides gras saturés",
-    'sugars': "Sucres"
+    'sugars': "Sucres",
+    "sodium": "Sodium",
+    "proteins": "Protéines",
+    "energy": "Energie",
+    "carbohydrates": "Glucides",
+    "fiber": "Fibres"
   }
+  public nutritionalValues;
+  public servingSize = "100g";
+  public show100g = false;
 
   constructor(private nav: NavController,
               private navParams: NavParams,
@@ -26,14 +34,50 @@ export class DetailsPage {
 
     this.product = navParams.get('product');
 
+    if (this.product.serving_size != "") this.servingSize = this.product.serving_size;
+
+    this.nutritionalValues = this.parseNutriments();
+
     this.product.additives_tags.forEach((tag) => {
       let ecode = tag.split(':')[1].toUpperCase();
       this.populateAdditives(tag, ecode);
     })
   }
 
-  getNutrients() {
+  getNutrientsLevels() {
     return Object.keys(this.product.nutrient_levels);
+  }
+
+  getNutrients() {
+    return Object.keys(this.product.nutriments)
+      .filter((key) => key.split('_').length == 1)
+      .filter((key) => key != "nutrition-score-fr" && key != "nutrition-score-uk");
+  }
+
+  parseNutriments() {
+    const nutriments = this.product.nutriments;
+    const keys = Object.keys(nutriments);
+    const primitives = this.getNutrients();
+    let parsed = {};
+
+    primitives.forEach((key) => {
+      let secondaries = keys.filter((secondKey) => {
+        let splitted = secondKey.split('_');
+
+        return splitted.length == 2 && splitted[0] == key;
+      }).map((filteredKey) => {
+        return filteredKey.split('_')[1];
+      })
+
+      let result = {};
+      secondaries.forEach((secondary) => {
+        result[secondary] = nutriments[`${key}_${secondary}`];
+      })
+
+      parsed[key] = result;
+    })
+
+    return parsed;
   }
 
   populateAdditives(tag, ecode) {
@@ -42,7 +86,7 @@ export class DetailsPage {
       data => {
         additive = data.json()[0];
 
-        if(additive == null) {
+        if (additive == null) {
           additive = {
             "code": ecode,
             "name": "Additif non réportorié",

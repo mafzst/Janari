1fd9165c304b50fecff4055bc5bb14adcb451b23
Nicolas <n.perraut@gmail.com>
2016-09-10 22:04:34 +0200

    Rebasing

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,11 @@
-import {Component} from '@angular/core';
+
+import {Component, PLATFORM_PIPES} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {StorageService} from './services/storage';
 import {HomePage} from './pages/home/home';
+import {TranslateLoader, TranslateStaticLoader, TranslateService, TranslatePipe} from "ng2-translate";
+import {Http} from "@angular/http";
 
 let prodMode: boolean = !!window.hasOwnProperty('cordava');
 
@@ -13,8 +16,11 @@ export class MyApp {
 
   private rootPage: any;
 
-  constructor(private platform: Platform) {
+  constructor(private platform: Platform, translate: TranslateService) {
     this.rootPage = HomePage;
+
+    translate.use('fr');
+    console.log(translate.getLangs());
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -24,4 +30,16 @@ export class MyApp {
   }
 }
 
-ionicBootstrap(MyApp, [], {prodMode: prodMode});
+ionicBootstrap(MyApp, [
+  {
+    provide: TranslateLoader,
+    useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
+    deps: [Http]
+  },
+  {
+    provide: PLATFORM_PIPES,
+    useValue: TranslatePipe,
+    multi: true
+  },
+  TranslateService
+], {prodMode: prodMode});

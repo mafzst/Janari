acbfc5ffc793d7f85539cc07730ff65cde9c76c2
Nicolas <n.perraut@gmail.com>
2016-09-30 23:16:00 +0200

    Readd Cloud module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import {NgModule} from '@angular/core';
 import {IonicApp, IonicModule} from 'ionic-angular';
+import {CloudSettings, CloudModule} from '@ionic/cloud-angular';
 import {Storage} from '@ionic/storage';
 import {MyApp} from './app.component';
 import {HomePage} from '../pages/home/home';
+
+const cloudSettings: CloudSettings = {
+    'core': {
+        app_id: 'd33141dc'
+    }
+}
 
 @NgModule({
     declarations: [
@@ -10,7 +17,8 @@ import {HomePage} from '../pages/home/home';
         HomePage
     ],
     imports: [
-        IonicModule.forRoot(MyApp)
+        IonicModule.forRoot(MyApp),
+        CloudModule.forRoot(cloudSettings)
     ],
     bootstrap: [IonicApp],
     entryComponents: [
